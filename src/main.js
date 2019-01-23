dfb792e43992392eac6dc7decc12ac78772f739b
huang-min <huang.min@navercorp.com>
2019-01-23 16:30:19 +0800

    babel config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import 'babel-polyfill'
 
 import router from './router/index.js'
 import store from './store/index.js'
